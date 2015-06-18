bcecccb3a6c19e3efec8c3283e2b998eb3276e5f
nathan3o4 <nathan3o4@gmail.com>
2015-06-18 09:46:06 -0500

    Messing with the credits page

--- Sunset/Sunset/CreditsViewController.m
+++ Sunset/Sunset/CreditsViewController.m
@@ -16,23 +16,24 @@
   blackGradient = [BackgroundLayer blackGradient];
   blackGradient.frame = self.view.bounds;
   [self.view.layer insertSublayer:blackGradient atIndex:0];
+  self.navigationController.navigationBarHidden = YES;
 }
 
-- (void)viewWillAppear:(BOOL)animated {
-  [[UIApplication sharedApplication] setStatusBarHidden:YES withAnimation:UIStatusBarAnimationNone];
-  originalColor = self.navigationController.navigationBar.barTintColor;
-  originalTentColor = self.navigationController.navigationBar.tintColor;
-  self.navigationController.navigationBar.barTintColor = [UIColor blackColor];
-  self.navigationController.navigationBar.translucent = NO;
-  self.navigationController.navigationBar.tintColor = [UIColor whiteColor];
-}
-
-- (void)viewWillDisappear:(BOOL)animated {
-  [[UIApplication sharedApplication] setStatusBarHidden:NO withAnimation:UIStatusBarAnimationNone];
-  self.navigationController.navigationBar.barTintColor = originalColor;
-  self.navigationController.navigationBar.translucent = YES;
-  self.navigationController.navigationBar.tintColor = originalTentColor;
-}
+//- (void)viewWillAppear:(BOOL)animated {
+//  [[UIApplication sharedApplication] setStatusBarHidden:YES withAnimation:UIStatusBarAnimationNone];
+//  originalColor = self.navigationController.navigationBar.barTintColor;
+//  originalTentColor = self.navigationController.navigationBar.tintColor;
+//  self.navigationController.navigationBar.barTintColor = [UIColor blackColor];
+//  self.navigationController.navigationBar.translucent = NO;
+//  self.navigationController.navigationBar.tintColor = [UIColor whiteColor];
+//}
+//
+//- (void)viewDidDisappear:(BOOL)animated {
+//  [[UIApplication sharedApplication] setStatusBarHidden:NO withAnimation:UIStatusBarAnimationNone];
+//  self.navigationController.navigationBar.barTintColor = originalColor;
+//  self.navigationController.navigationBar.translucent = YES;
+//  self.navigationController.navigationBar.tintColor = originalTentColor;
+//}
 
 - (IBAction)dismissCreditsView:(id)sender {
   [self dismissViewControllerAnimated:YES completion:nil];
