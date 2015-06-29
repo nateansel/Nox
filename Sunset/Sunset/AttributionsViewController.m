91846b73c063dd66521cf030a980f8fd52c0fbaf
Chase McCoy <chasem000@gmail.com>
2015-06-29 17:03:47 -0500

    Fixed credits page and cleaned up some visual bugs

--- Sunset/Sunset/AttributionsViewController.m
+++ Sunset/Sunset/AttributionsViewController.m
@@ -11,15 +11,31 @@
 @implementation AttributionsViewController
 
 - (void)viewDidLoad {
+//  NSString *htmlFile = [[NSBundle mainBundle] pathForResource:@"attributions" ofType:@"html"];
+//  NSString* htmlString = [NSString stringWithContentsOfFile:htmlFile encoding:NSUTF8StringEncoding error:nil];
+//  
+//  NSString *path = [[NSBundle mainBundle] bundlePath];
+//  NSURL *baseURL = [NSURL fileURLWithPath:path];
+//  [_webView loadHTMLString:htmlString baseURL:baseURL];
+//  
+//  CALayer *blueGradient = [BackgroundLayer blueGradient];
+//  blueGradient.frame = self.view.frame;
+//  [self.view.layer insertSublayer:blueGradient atIndex:0];
+//  [self.navigationController.navigationBar setBackgroundImage:[UIImage imageNamed:@"navBarBackground" ] forBarMetrics:UIBarMetricsDefault];
+//  [self.navigationController.navigationBar setTintColor:[UIColor whiteColor]];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+  [super viewWillAppear:animated];
+  
+  [self.navigationController.navigationBar setTintColor:[UIColor whiteColor]];
+  
   NSString *htmlFile = [[NSBundle mainBundle] pathForResource:@"attributions" ofType:@"html"];
   NSString* htmlString = [NSString stringWithContentsOfFile:htmlFile encoding:NSUTF8StringEncoding error:nil];
-  [_webView loadHTMLString:htmlString baseURL:nil];
   
-  CALayer *blueGradient = [BackgroundLayer blueGradient];
-  blueGradient.frame = self.view.frame;
-  [self.view.layer insertSublayer:blueGradient atIndex:0];
-  [self.navigationController.navigationBar setBackgroundImage:[UIImage imageNamed:@"navBarBackground" ] forBarMetrics:UIBarMetricsDefault];
-  [self.navigationController.navigationBar setTintColor:[UIColor whiteColor]];
+  NSString *path = [[NSBundle mainBundle] bundlePath];
+  NSURL *baseURL = [NSURL fileURLWithPath:path];
+  [_webView loadHTMLString:htmlString baseURL:baseURL];
 }
 
 @end
