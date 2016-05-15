fbb5bfabf024d253d5a35586547a14c7b835fa43
Chase McCoy <chasem000@gmail.com>
2016-05-15 16:06:03 -0500

    Added SunEvent

---
+++ Nox/Nox/SunEvent.swift
@@ -0,0 +1,14 @@
+//
+//  SunEvent.swift
+//  Nox
+//
+//  Created by Chase McCoy on 5/15/16.
+//  Copyright © 2016 Chase McCoy. All rights reserved.
+//
+
+import UIKit
+
+enum SunEvent {
+  case Sunrise(NSDate)
+  case Sunset(NSDate)
+}
