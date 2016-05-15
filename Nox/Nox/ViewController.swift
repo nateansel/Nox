31572cf6b9b8ddbe6d8a85a4e8cd584855ed4abd
Chase McCoy <chasem000@gmail.com>
2016-05-15 14:25:49 -0500

    Added new project

---
+++ Nox/Nox/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Nox
+//
+//  Created by Chase McCoy on 5/15/16.
+//  Copyright © 2016 Chase McCoy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+}
+
