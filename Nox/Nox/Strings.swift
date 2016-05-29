6e9591582552fc57ccc2f81f5eb3fe3d41e8303b
nathan3o4 <nathan3o4@gmail.com>
2016-05-29 16:43:25 -0500

    Added initial Strings file for having globalized strings for accessing NSUserDefaults

---
+++ Nox/Nox/Strings.swift
@@ -0,0 +1,16 @@
+//
+//  Strings.swift
+//  Nox
+//
+//  Created by Nathan Ansel on 5/29/16.
+//  Copyright © 2016 Chase McCoy. All rights reserved.
+//
+
+import Foundation
+
+enum Strings {
+  enum Settings {
+    static let twelveHourTime = "twelveHourTime"
+    static let notificationsArray = "notificationsArray"
+  }
+}
