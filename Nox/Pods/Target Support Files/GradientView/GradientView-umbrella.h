206b62e77a4ff92ed8aec14a7482a68dbcc8aa12
Chase McCoy <chasem000@gmail.com>
2016-07-31 14:14:14 -0500

    ~

--- Nox/Pods/Target Support Files/GradientView/GradientView-umbrella.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "GradientView.h"
-
-FOUNDATION_EXPORT double GradientViewVersionNumber;
-FOUNDATION_EXPORT const unsigned char GradientViewVersionString[];
-
--- Nox/Pods/Target Support Files/Pods-Nox/Pods-Nox-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Nox : NSObject
-@end
-@implementation PodsDummy_Pods_Nox
-@end
