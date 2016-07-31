98a9fc60a8ff7783fdbc218b128e345d10319aa3
nathan3o4 <nathan3o4@gmail.com>
2016-07-31 14:20:08 -0500

    Fixes an issue with cocoapods

---
+++ Nox/Pods/Target Support Files/Pods-Nox Widget/Pods-Nox Widget-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Nox_Widget : NSObject
+@end
+@implementation PodsDummy_Pods_Nox_Widget
+@end
---
+++ Nox/Pods/Target Support Files/Pods-Nox Widget/Pods-Nox Widget-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Nox_WidgetVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Nox_WidgetVersionString[];
+
